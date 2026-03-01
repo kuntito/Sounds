cbc6a5acd09005a0f34b87ba918db20e4b5960d3
kuntito <owolabitito@gmail.com>
2026-03-01 16:20:17 +0000

    used AppTextButton for create playlist button

--- app/src/main/java/com/example/sounds/ui/components/playlist_list/CreatePlaylistButton.kt
+++ app/src/main/java/com/example/sounds/ui/components/playlist_list/CreatePlaylistButton.kt
@@ -1,14 +1,10 @@
 package com.example.sounds.ui.components.playlist_list
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.sounds.ui.components.utils.ClickableSurface
+import com.example.sounds.ui.components.utils.AppTextButton
 import com.example.sounds.ui.components.utils.PreviewColumn
-import com.example.sounds.ui.theme.tsOrion
 
 @Composable
 fun CreatePlaylistButton(
@@ -16,23 +12,11 @@ fun CreatePlaylistButton(
     onClick: () -> Unit,
 ) {
 
-    ClickableSurface (
+    AppTextButton(
+        text = "create playlist",
         onClick = onClick,
-        isRippleBounded = true,
-        modifier = modifier
-        ,
-    ) {
-        Text(
-            "create playlist",
-            style = tsOrion,
-            modifier = Modifier
-                .padding(
-                    vertical = 8.dp,
-                    horizontal = 4.dp,
-                )
-            ,
-        )
-    }
+        modifier = modifier,
+    )
 }
 
 @Preview
